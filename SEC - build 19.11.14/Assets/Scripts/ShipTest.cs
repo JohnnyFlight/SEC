98f6c85a4c099398897d8ca682c3b19ec88fcd37
JohnnyFlight <fire-song@hotmail.co.uk>
2015-06-09 15:28:28 +0100

    Asteroids split appropriately
    Still need to make them split when collided with and have the split
    direction be based on where they were hit.

---
+++ SEC - build 19.11.14/Assets/Scripts/ShipTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			transform.rigidbody.AddForce(transform.right * 100.0f);
+		}
+	}
+}
